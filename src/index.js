5e51e3a338d58b19fa156c875ba37c8fd0a410e4
Agbasi chukwudum <chukwuduma@yahoo.com>
2024-09-08 15:49:48 -0700

    REACT SIMPLE PORFOLIO

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <div className="skill-list">
+          <SkillList skill="HTML+CSS" color="blue" />
+
+          <SkillList skill="JavaScript" color="yellow" />
+
+          <SkillList skill="Web Design" color="green" />
+
+          <SkillList skill="Git and GitHub" color="red" />
+
+          <SkillList skill="React" color="skyblue" />
+
+          <SkillList skill="Svelte" color="red" />
+        </div>
+      </div>
+    </div>
+  );
+
+  function Avatar() {
+    const style = { width: "100%", height: "300px" };
+
+    return <img style={style} src="picture/IMG_0007.JPG" alt="portfolio" />;
+  }
+
+  function Intro() {
+    return (
+      <div>
+        <h1>AGBASI CHUKWUDUM</h1>
+        <p>
+          Full-stack web developer and aspiring Tech starup CEO. when not coding
+          i love to watch anime, cook and enjoy R&B songs.{" "}
+        </p>
+      </div>
+    );
+  }
+
+  function SkillList(props) {
+    return (
+      <div className="skill" style={{ backgroundColor: props.color }}>
+        {props.skill}
+      </div>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
